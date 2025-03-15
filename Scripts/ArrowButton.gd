e9dfad471806b9970e6dc7b6b39f41ddbbf57a79
Michael Rehmet <mrehmet@Michaels-MacBook-Pro-7.local>
2025-03-15 14:34:20 -0400

    progress on sync phase

--- Scripts/ArrowButton.gd
+++ Scripts/ArrowButton.gd
@@ -13,17 +13,17 @@ func _unhandled_input(event):
 		if event.is_action_pressed(input, false):
 			if current_note != null:
 				if perfect:
-					get_parent().increment_score(3)
+					get_parent().get_parent().increment_score(3)
 					current_note.destroy(3)
 				elif good:
-					get_parent().increment_score(2)
+					get_parent().get_parent().increment_score(2)
 					current_note.destroy(2)
 				elif okay:
-					get_parent().increment_score(1)
+					get_parent().get_parent().increment_score(1)
 					current_note.destroy(1)
 				_reset()
 			else:
-				get_parent().increment_score(0)
+				get_parent().get_parent().increment_score(0)
 		if event.is_action_pressed(input):
 			frame = 1
 		elif event.is_action_released(input):
