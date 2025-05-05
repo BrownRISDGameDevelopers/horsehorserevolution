4cfb1ad7b27cb1b9943513495c9ed4db966e0de4
Brian Xu <xu.brian.s@gmail.com>
2025-05-05 14:14:14 -0400

    dynamic slip

--- Scenes/game_scene/game_content/player_horse.gd
+++ Scenes/game_scene/game_content/player_horse.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const MISS = 4
 
 class DoubleLinkedList:
 	var prev_node: DoubleLinkedList = null
@@ -45,6 +46,13 @@ class DoubleLinkedList:
 			var _new_node = DoubleLinkedList.new(_state, self.last_node())
 
 var current_states: Array[DoubleLinkedList] = [DoubleLinkedList.new(Global.Direction.LEFT), DoubleLinkedList.new(Global.Direction.LEFT)]
+var tape_centroid := Vector2(102, 467) / 2
+
+@onready var back_anchor_top = $PlayerBack/PlayerBackNeutral/AnchorTop
+@onready var back_anchor_bottom = $PlayerBack/PlayerBackNeutral/AnchorBottom
+@onready var front_anchor_top = $PlayerFront/PlayerFrontNeutral/AnchorTop
+@onready var front_anchor_bottom = $PlayerFront/PlayerFrontNeutral/AnchorBottom
+	
 
 @export var back_sprites: Array[Sprite2D]
 @export var front_sprites: Array[Sprite2D]
@@ -54,6 +62,7 @@ var current_states: Array[DoubleLinkedList] = [DoubleLinkedList.new(Global.Direc
 
 func _ready():
 	Global.note_hit.connect(check_miss)
+	stretch_tape()
 
 func _physics_process(_delta):
 	for player in Global.PlayerEnum:
@@ -62,9 +71,10 @@ func _physics_process(_delta):
 			if Input.is_action_just_pressed(input_name):
 				current_states[Global.PlayerEnum[player]].add_state(Global.Direction[direction])
 				reset_timers[Global.PlayerEnum[player]].stop()
-			if Input.is_action_just_released(input_name):
+				set_visibility(Global.PlayerEnum[player])
+			elif Input.is_action_just_released(input_name):
 				current_states[Global.PlayerEnum[player]].pop_state(Global.Direction[direction])
-		set_visibility(Global.PlayerEnum[player])
+				set_visibility(Global.PlayerEnum[player])
 
 func set_visibility(player: Global.PlayerEnum):
 	var sprite_list
@@ -80,40 +90,62 @@ func set_visibility(player: Global.PlayerEnum):
 		for sprite in sprite_list:
 			sprite.visible = false
 		sprite_list[player_node.state].visible = true
+		if player == Global.PlayerEnum.PLAYER_1:
+			back_anchor_top = sprite_list[player_node.state].get_node("AnchorTop")
+			back_anchor_bottom = sprite_list[player_node.state].get_node("AnchorBottom")
+		else:
+			front_anchor_top = sprite_list[player_node.state].get_node("AnchorTop")
+			front_anchor_bottom = sprite_list[player_node.state].get_node("AnchorBottom")
+	stretch_tape()
 
-func reset_pose(sprite_list):
+func stretch_tape():
+	$PlayerTape.polygon = [to_local(back_anchor_top.global_position) + tape_centroid,
+							to_local(front_anchor_top.global_position) + tape_centroid,
+							to_local(front_anchor_bottom.global_position) + tape_centroid,
+							to_local(back_anchor_bottom.global_position) + tape_centroid]
+
+func reset_pose(sprite_list, player):
 	for sprite in sprite_list:
 		sprite.visible = false
 	sprite_list[-1].visible = true
+	if player == Global.PlayerEnum.PLAYER_1:
+		back_anchor_top = $PlayerBack/PlayerBackNeutral/AnchorTop
+		back_anchor_bottom = $PlayerBack/PlayerBackNeutral/AnchorBottom
+	else:
+		front_anchor_top = $PlayerFront/PlayerFrontNeutral/AnchorTop
+		front_anchor_bottom = $PlayerFront/PlayerFrontNeutral/AnchorBottom
+	stretch_tape()
+
 
 func _on_back_timer_timeout():
-	reset_pose(back_sprites)
+	reset_pose(back_sprites, Global.PlayerEnum.PLAYER_1)
 
 func _on_front_timer_timeout():
-	reset_pose(front_sprites)
+	reset_pose(front_sprites, Global.PlayerEnum.PLAYER_2)
 
 
 func check_miss(player, area, _score):
 	if area == Global.AreaHit.MISS:
 		miss(player)
+		stretch_tape()
 
 func miss(player):
 	if player == Global.PlayerEnum.PLAYER_1:
-		$PlayerBack.visible = false
-		$PlayerBackMiss.visible = true
 		$BackMissTimer.start(pose_duration)
 	else:
-		$PlayerFront.visible = false
-		$PlayerFrontMiss.visible = true
 		$FrontMissTimer.start(pose_duration)
+	current_states[player].add_state(MISS)
+	set_visibility(player)
 
-func _on_front_miss_timer_timeout():
-	$PlayerFront.visible = true
-	$PlayerFrontMiss.visible = false
+func reset_miss(player):
+	current_states[player].pop_state(MISS)
+	set_visibility(player)
 
 func _on_back_miss_timer_timeout():
-	$PlayerBack.visible = true
-	$PlayerBackMiss.visible = false
+	reset_miss(Global.PlayerEnum.PLAYER_1)
+
+func _on_front_miss_timer_timeout():
+	reset_miss(Global.PlayerEnum.PLAYER_2)
 
 	
 func slip(permanent = false):
@@ -128,8 +160,6 @@ func slip(permanent = false):
 func set_slip_visibility(is_slipped):
 	$PlayerBack.visible = !is_slipped
 	$PlayerFront.visible = !is_slipped
-	$PlayerBackMiss.visible = false
-	$PlayerFrontMiss.visible = false
 	$PlayerConnector.visible = !is_slipped
 	$PlayerTape.visible = !is_slipped
 	$PlayerSlip.visible = is_slipped
